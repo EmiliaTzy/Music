ed762c2f7c3eb251e9ad8a47bfec9a7993a47b39
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-22 18:10:02 +0700

    Update playfrom.py

--- Ankit/playfrom.py
+++ Ankit/playfrom.py
@@ -13,7 +13,7 @@ async def playfrom(client, m: Message):
  if GRPPLAY or (m.from_user and m.from_user.is_contact) or m.outgoing:
    chat_id = m.chat.id
    if len(m.command) < 2:
-      await m.reply("**USAGE:** \n\n`/playfrom [chat_id/username]` \n`/playfrom [chat_id/username] ; [no. of songs]`")
+      await m.reply("**PENGGUNAAN:** \n\n`/playfrom [chat_id/username]` \n`/playfrom [chat_id/username] ; [no. of songs]`")
    else:
       args = m.text.split(maxsplit=1)[1]
       if ";" in args:
@@ -22,7 +22,7 @@ async def playfrom(client, m: Message):
       else:
          chat = args
          limit = 10
-      hmm = await m.reply(f"Searching the last **{limit}** Songs from `{chat}`")
+      hmm = await m.reply(f"Pencarian Terakhir **{limit}** Lagu dari `{chat}`")
       try:
          async for x in bot.search_messages(chat, limit=limit, filter="audio"):
                location = await x.download()
@@ -45,8 +45,8 @@ async def playfrom(client, m: Message):
                      stream_type=StreamType().pulse_stream,
                   )
                   add_to_queue(chat_id, songname, location, link, "Audio", 0)
-                  await m.reply(f"**Ankit Started Playing Songs from {chat} ▶** \n**🎧 SONG** : [{songname}]({link}) \n**💬 CHAT** : `{chat_id}`", disable_web_page_preview=True)
+                  await m.reply(f"**Memulai Memainkan Lagu{chat} ▶** \n**🎧 Lagu** : [{songname}]({link}) \n**💬 Obrolan** : `{chat_id}`", disable_web_page_preview=True)
          await hmm.delete()
-         await m.reply(f"Added **{limit}** SONGS to Queue")
+         await m.reply(f"Menambahkan **{limit}** Lagu Ke Antrian")
       except Exception as e:
          await hmm.edit(f"**ERROR** \n`{e}`")
