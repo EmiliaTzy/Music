17bd220594d2d8014719b87e078ebd25c097229f
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-22 18:10:55 +0700

    Update playlist.py

--- Ankit/playlist.py
+++ Ankit/playlist.py
@@ -11,9 +11,9 @@ async def playlist(client, m: Message):
    if chat_id in QUEUE:
       chat_queue = get_queue(chat_id)
       if len(chat_queue)==1:
-         await m.reply(f"**🎧 NOW ANKIT IS PLAYING:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", disable_web_page_preview=True)
+         await m.reply(f"**🎧 Memulai Memainkan Lagu:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", disable_web_page_preview=True)
       else:
-         QUE = f"**🎧 NOW ANKIT IS PLAYING:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ PLAYLIST:**"
+         QUE = f"**🎧 Memulai Memainkan Lagu:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ PLAYLIST:**"
          l = len(chat_queue)
          for x in range (1, l):
             hmm = chat_queue[x][0]
@@ -22,4 +22,4 @@ async def playlist(client, m: Message):
             QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`"
          await m.reply(QUE, disable_web_page_preview=True)
    else:
-      await m.reply("`Nothing is Streaming`")
+      await m.reply("`Tidak Ada Streaming`")
